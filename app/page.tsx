d6e5f475a96ea9b3691c30677a977c79171c5c7e
humemaisrar-github-account <162582825+humemaisrar-github-account@users.noreply.github.com>
2024-09-25 08:38:11 -0700

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "./component/Header";
+import About from "./component/About";
+ 
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Header/>
+      <About/>
+    </div>
+  );
+}
